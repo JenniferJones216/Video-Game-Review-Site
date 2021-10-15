7f72c65397c1f13c8b6bff4ca8824e80a7d237bd
jfeller403 <jfeller403@gmail.com>
2021-10-15 11:34:25 -0400

    added Navbar again

--- ReviewsSite/Migrations/20211015151922_moreSeedData.Designer.cs
+++ ReviewsSite/Migrations/20211015151922_moreSeedData.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReviewsSite;
 
 namespace ReviewsSite.Migrations
 {
     [DbContext(typeof(VideoGameContext))]
-    partial class VideoGameContextModelSnapshot : ModelSnapshot
+    [Migration("20211015151922_moreSeedData")]
+    partial class moreSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
